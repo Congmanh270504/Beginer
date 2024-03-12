6e3febc94fd0e2ee5e3a0fe3716f77e4f05ae3bd
Trần Công Mạnh <120402571+Congmanh270504@users.noreply.github.com>
2024-03-12 23:36:53 +0700

    update

---
+++ Beginer/ElectricBill/main.cs
@@ -0,0 +1,80 @@
+﻿//using System;
+//crl R+ E shortcut get set atribute
+//namespace Beginer // Note: actual namespace depends on the project name.
+//{
+
+//    class Program
+//    {
+
+//        static void Main(string[] args)
+//        {
+//            //Student_Generic First = new Student_Generic();
+//            Student_Generic list = new Student_Generic();
+
+//            list.Add(new Student("4312a", "Manh", 9.0));
+//            list.Add(new Student("765B", "Son", 8.0));
+//            list.Add(new Student("078C", "Quy", 7.5));
+//            list.Add(new Student("1234D", "Tan", 10));
+
+//            list.xuatDS();
+
+//            list.getGreater8();
+
+//            Console.Write("Nhap ten sinh vien can tim: ");
+//            string a = Console.ReadLine()!;
+//            list.findStudentByName(a);
+
+//            Console.Write("Nhap ma sinh vien can tim: ");
+//            a = Console.ReadLine()!;
+//            list.findStudentById(a);
+//            int choose = 0;
+//            do
+//            {
+//                Console.WriteLine("Nhap lua chon bai lam:");
+//                choose = int.Parse(Console.ReadLine());
+//                switch (choose)
+//                {
+//                    case 1:
+//                        break;
+//                    case 2:
+//                        break;
+//                    default:
+//                        Console.WriteLine("Khong co su lua chon do");
+//                        break;
+//                }
+//            }
+//            while (choose != 0);
+
+
+
+
+
+//            EmployeeList employeeList = new EmployeeList();
+//            employeeList.Add(new Employee("4312a", "Manh", 1.0, 2013));
+//            employeeList.Add(new Employee("765B", "Son", 2.0, 2008));
+//            employeeList.Add(new Employee("078C", "Quy", 3.5, 2015));
+//            employeeList.Add(new Employee("1234D", "Tan", 4.0, 2018));
+
+//            employeeList.getInfor();
+//            employeeList.checkSalary();
+
+//            Console.WriteLine("Nhan vien co tien luong cao nhat thang: ");
+//            employeeList.indexOf(employeeList.getHighestSalary());
+//            employeeList.sortDateWorkIncrease();
+//            employeeList.getInfor();
+//            employeeList.Add(new Employee("4312a", "Manh", 9.0, 2012));
+//            employeeList.Add(new Employee("765B", "Son", 8.0, 2013));
+//            employeeList.Add(new Employee("078C", "Quy", 7.5, 2015));
+//            employeeList.Add(new Employee("1234D", "Tan", 10, 2014));
+
+//            employeeList.getInfor();
+//            Console.WriteLine("Tong tien luong nhan vien: " + employeeList.getSumSalary());
+//            Console.WriteLine("Nhap ten nhan vien can tim: ");
+
+//            Console.ReadKey();
+
+//        }
+//    }
+
+//}
+
