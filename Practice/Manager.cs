a152f0ac620b8af3ccae068b84325eb961d3b36c
Trần Công Mạnh <120402571+Congmanh270504@users.noreply.github.com>
2024-05-02 10:47:54 +0700

    Add files via upload

---
+++ Practice/Manager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beginer
+{
+    class Manager : ABC
+    {
+        string position;
+        double positionpoint;
+        public Manager(string id, string name, string degree, string place, double salarypoint, int error, string position, double positionpoint)
+            : base(id, name, degree, place, salarypoint, error)
+        {
+            this.position = position;
+            this.positionpoint = positionpoint;
+        }
+        public override double bonus()
+        {
+            return positionpoint * basesalary;
+        }
+        public override void getInfor()
+        {
+            base.getInfor();
+            Console.WriteLine("Position:{0} Position Point: {1}", position, positionpoint);
+        }
+    }
+}
