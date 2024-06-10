4ff4a93aacc525e74b67cd0ff82e9b7dc2cace3a
Trần Công Mạnh <120402571+Congmanh270504@users.noreply.github.com>
2024-06-10 22:06:20 +0700

    #3-update base

---
+++ Base/ListTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+namespace Base
+{
+    internal class ListTest
+    {
+        List<Class1> ts = new List<Class1>();
+        XmlDocument read = new XmlDocument();
+
+        internal List<Class1> Ts { get => ts; set => ts = value; }
+        public void Input()
+        {
+            read.Load("");
+            XmlNodeList list = read.SelectNodes("");
+            foreach (XmlNode item in list)
+            {
+                T temp;
+                string type = item["type"].InnerText;
+                string id = item["id"].InnerText;
+                string name = item["name"].InnerText;
+                int dayoff = int.Parse(item["dayoff"].InnerText);
+                int timestart = int.Parse(item["timestart"].InnerText);
+                double salaryPoint = double.Parse(item["salaryPoint"].InnerText);
+                switch (type)
+                {
+                    case "Production":
+                        temp = new Production(id, name, salaryPoint, timestart, dayoff, double.Parse(item["heabyJob"].InnerText));
+                        break;
+                    case "Business":
+                        temp = new Business(id, name, salaryPoint, timestart, dayoff, int.Parse(item["minitem"].InnerText), int.Parse(item["item"].InnerText));
+                        break;
+                    case "Manager":
+                        temp = new Manager(id, name, salaryPoint, timestart, dayoff, item["position"].InnerText, double.Parse(item["positionPoint"].InnerText));
+                        break;
+                    default:
+                        throw new Exception("cc loi cai lz a` ");
+                }
+                listT.Add(temp);
+            }
+            list = read.SelectNodes("/ListT/BCD");
+            foreach (XmlNode item in list)
+            {
+                BCD bcd = new BCD();
+                bcd.Id = item["id"].InnerText;
+                bcd.Name = item["name"].InnerText;
+                bcd.Total = double.Parse(item["total"].InnerText);
+                listT.Add(bcd);
+            }
+        }
+        public void Output()
+        {
+            foreach (var item in ts)
+            {
+                Console.WriteLine("");
+            }
+        }
+        public void menu()
+        {
+            Console.WriteLine("============================= Menu ==============================");
+            Console.WriteLine("|| *********************** Show List ************************* ||");
+            Console.WriteLine("|| * 1.Show Cutomer list                                     * ||");
+            Console.WriteLine("|| * 2.Show Diver list                                       * ||");
+            Console.WriteLine("|| ************************** Exam *************************** ||");
+            Console.WriteLine("|| * 3.Count in total vehical                                * ||");
+            Console.WriteLine("|| * 4.Count total customer                                  * ||");
+            Console.WriteLine("|| ************************** Exit *************************** ||");
+            Console.WriteLine("|| * 0.Exit                                                  * ||");
+            Console.WriteLine("|| ************************** End *****************************||");
+            Console.WriteLine("=================================================================");
+        }
+    }
+}
---
+++ Base/main.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace Base
+{
+    internal class main
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            int choose = 0;
+            double result = 0;
+            ListTest listTest = new ListTest();
+            //listTest.Input();
+            listTest.menu();
+            do
+            {
+                Console.Write("Choose option: ");
+                choose = Convert.ToInt32(Console.ReadLine());
+                switch (choose)
+                {
+                    case 1:
+                        listTest.Output();
+                        break;
+                    case 2:
+                       
+                        break;
+                    case 3:
+
+                        break;
+                    case 0:
+                        Console.WriteLine("\tHave a nice day sir!!\n");
+                        break;
+                    default:
+                        Console.WriteLine("Don't have that choose!!\n");
+                        break;
+                }
+            } while (choose != 0);
+            Console.ReadKey();
+        }
+    }
+}
